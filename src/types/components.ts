f27fc3fdad989eb0464c82f0d2c7500d69f42a10
Shalev <Shalev0125@gmail.com>
2022-07-28 14:08:27 +0300

    added missing imports

--- src/types/components.ts
+++ src/types/components.ts
@@ -1,3 +1,5 @@
+
+import React from 'react';
 import type { SelectInput, NumberInput, TextInput } from './inputs';
 import type { StyleElements } from './style';
 
--- src/types/style.ts
+++ src/types/style.ts
@@ -1,3 +1,4 @@
+import React from 'react';
 
 type StyleElements = Partial<{
     className: string;
