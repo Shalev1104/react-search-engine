b9f053312febb25dad2a46ebc16769150f933c78
Shalev <Shalev0125@gmail.com>
2022-08-13 19:00:33 +0300

    fixed style and other bugs

--- src/types/components.ts
+++ src/types/components.ts
@@ -2,12 +2,13 @@
 import React from 'react';
 import type { Inputs } from './inputs'
 import type { StyleElements } from './style';
+import type { ReducerState } from './reducer';
 
 type SearchProvider = {
     children: React.ReactNode;
-    onFilter?: (filtered: any) => void
+    onFilter?: (filtered: ReducerState) => void
 } & StyleElements;
 
-type SearchBarField = Inputs & StyleElements;
+type SearchBarField = Inputs
 
 export type { SearchProvider, SearchBarField };
